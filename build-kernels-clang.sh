a14312712661937cb2d65b5fc80330d47ea0cb82
Alin Jerpelea <alin.jerpelea@sony.com>
2022-08-24 14:40:55 +0200

    build-kernels-clang.sh: add Android 13 Clang
    Add clang 14.0.5 from Android 13
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- build-kernels-clang.sh
+++ build-kernels-clang.sh
@@ -5,13 +5,17 @@
 
 CLANG_A11=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r353983c/bin/
 CLANG_A12=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r416183b/bin/
+CLANG_A13=$ANDROID_ROOT/prebuilts/clang/host/linux-x86/clang-r450784d/bin/
 
-if [ -d "$CLANG_A11" ]; then
-    echo "Using Clang (build r353983) from Android 11."
-    export CLANG=$CLANG_A11
+if  [ -d "$CLANG_A13" ]; then
+    echo "Using Clang (build r450784d) from Android 13."
+    export CLANG=$CLANG_A13
 elif  [ -d "$CLANG_A12" ]; then
     echo "Using Clang (build r416183b) from Android 12."
     export CLANG=$CLANG_A12
+elif [ -d "$CLANG_A11" ]; then
+    echo "Using Clang (build r353983) from Android 11."
+    export CLANG=$CLANG_A11
 fi
 
 # Cross Compiler
